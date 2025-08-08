d4ac800cd8b91a576a29920f018d1baabee1be7c
Sameer Nadaf <nsameernadaf@gmail.com>
2025-08-08 16:40:56 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sameer  on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
